78a7ca58a873b47d23aeb4c1e4a6aa0a48bc3b88
Quésia <quesiasts@gmail.com>
2020-07-31 12:26:55 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
       <BannerMain  videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}/>
+        videoDescription={"Fez um photoshop na minha vida, e me tirou da vida bandida que eu tinha pra essa mal resolvida. Podia ter durado um pouco mais!"}/>
 
       <Carousel 
         ignoreFirstVideo
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
+      <a href="/">
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
       <p>
